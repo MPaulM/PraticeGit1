a715de3bf82354b8fafde8d88e917771889d6bb6
Paul Morgan <psamoylov@gmail.com>
2020-11-10 13:02:50 -0500

    changed the file that i goy from github.com
    Signed-off-by: Paul Morgan <psamoylov@gmail.com>

--- src/day2/Practice2.java
+++ src/day2/Practice2.java
@@ -1,1 +1,12 @@
 //added new file
+package day2;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Test");
+        //doing more commits after publishing doing just push
+
+
+    }
+}
