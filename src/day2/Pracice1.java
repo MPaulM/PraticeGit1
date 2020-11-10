a513f09a917b835048760ba38865b8ea8257c57d
Paul Morgan <psamoylov@gmail.com>
2020-11-10 12:49:13 -0500

    project is shared and is in remote. commiting more changes
    Signed-off-by: Paul Morgan <psamoylov@gmail.com>

--- src/day2/Pracice1.java
+++ src/day2/Pracice1.java
@@ -4,6 +4,7 @@ public class Pracice1 {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Test");
+        //doing more commits after publishing doing just push
 
 
     }
